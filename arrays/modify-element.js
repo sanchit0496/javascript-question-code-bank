50275e89226736199d8ae68976026a7af256cfcc
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-15 15:32:12 +0530

    modify element is complete

--- arrays/modify-element.js
+++ arrays/modify-element.js
@@ -35,3 +35,64 @@ console.log(newArr3)
 let arr4 = [1,2,3,4]
 let newArr4 = [0].concat(arr4)
 console.log(newArr4)
+
+// 3) remove element from end of array 
+let arr5 = [1,2,3,4]
+let removedElement = arr5.pop()
+console.log(arr5)
+console.log(removedElement)
+
+let  arr6 = [1,2,3,4]
+arr6.length = arr6.length -1
+console.log(arr6)
+
+let  arr7 = [1,2,3,4]
+let removedElement1 = arr7.splice(-1, 1)
+console.log(arr7)
+console.log(removedElement1)
+
+let arr8 = [1,2,3,4]
+let newArr8 = arr8.slice(0, arr8.length-1)
+console.log(newArr8)
+
+// 4) Remove an element from the beginning of an array.
+
+let arr9 = [1,2,3,4]
+let removedElement9 = arr9.shift()
+console.log('arr9', arr9)
+console.log('removedElement9', removedElement9)
+
+let arr10 = [1,2,3,4]
+let removedElement10 = arr10.splice(0,1)
+console.log(arr10)
+console.log(removedElement10)
+
+let arr11 = [1,2,3,4]
+let newArray11 = arr11.slice(1)
+console.log(newArray11)
+
+let arr12 = [1,2,3,4]
+let newArr12 = arr12.filter((item, index) => index !== 0)
+console.log(newArr12)
+
+// 5) Modify an existing element in an array.
+let arr13 = [1,2,3,4]
+arr13[1] = 100
+console.log(arr13)
+
+let arr14 = [1,2,3,4]
+let newArr14 = arr14.map((item, index) => {
+    if(item === 2){
+        return 100
+    }
+    return item
+})
+console.log(newArr14)
+
+let arr15 = [1,2,3,4]
+for(let i = 0 ; i < arr15.length; i++){
+    if(i === 1){
+        arr15[i] = 100
+    }
+}
+console.log(arr15)
