024ba6f0ede01887f06ac81dfd1338c92c14cb36
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-17 23:27:40 +0530

    have completed object method here

--- arrays/object-methods.js
+++ arrays/object-methods.js
@@ -118,10 +118,129 @@ console.log(Object.assign({}, obj18))
 // Object.create
 
 // 1) Task: Use Object.create to create a new object that inherits from a prototype object { greet() { return "Hello"; } }.
- let obj19 = {
+ let protoObj = {
     greet: function(){
         return 'hello'
     }
  }
- let obj20 = Object.create(obj19)
+ let obj20 = Object.create(protoObj)
  console.log(obj20.greet())
+
+// 2) Task: Use Object.create to create a new object that inherits from a prototype and also has its own properties { name: "Alice" }.
+let proto1 = {
+    greet: function(){
+        return 'hello'
+    }
+}
+let obj21 = Object.create(proto1, {
+    name: {value: 'whatsup'}
+})
+console.log(obj21)
+
+
+// 3) Task: Use Object.create(null) to create an object that does not inherit from Object.prototype.
+let obj22 = Object.create(null)
+obj22.key = 'value'
+console.log(obj22)
+
+// Object.freeze
+
+// 1) Task: Use Object.freeze to freeze an object { name: "Alice", age: 30 }, preventing any modifications.
+let obj23 = {
+    name: 'alice'
+}
+console.log(obj23); //The empty object {} is logged because console.log only shows own properties of the object
+console.log(obj23.name); // it will return "alice" because it is inherited from proto2
+Object.freeze(obj23)
+obj23.name  = 'a'
+console.log(obj23)
+
+// 2) Question 2: Attempt to Add Properties to a Frozen Object
+let obj24 = { name: "Alice" };
+obj24.age = 12
+obj24.greet = 'hello'
+Object.freeze(obj24)
+obj24.marks = 40
+Object.freeze(obj24)
+console.log(obj24)
+
+// 3) Question 3: Attempt to Delete Properties from a Frozen Object
+delete obj24.greet
+console.log(obj24)
+
+// 4) Task: After freezing an object using Object.freeze, check if the object is actually frozen using Object.isFrozen.
+console.log(Object.isFrozen(obj24))
+
+
+//  Object.seal
+
+// 1) Task: Use Object.seal to seal an object { name: "Alice", age: 30 }, preventing new properties from being added but allowing modification of existing properties.
+let obj25 = {
+    name: 'hello'
+}
+console.log(obj25)
+Object.seal(obj25)
+obj25.name ='there'
+obj25.marks = 25
+console.log(obj25)
+
+// 2) Task: After sealing an object using Object.seal, check if the object is actually sealed using Object.isSealed.
+console.log(Object.isSealed(obj25))
+
+
+// Object.fromEntries
+
+// 1) Task: Use Object.fromEntries to convert an array of key-value pairs [["name", "Alice"], ["age", 30]] into an object.
+let entries = [["name", "Alice"], ["age", 30]];
+console.log(Object.fromEntries(entries))
+
+
+// Object.is
+
+// 1) Task: Use Object.is to compare two primitive values 5 and 5.
+let a = 5;
+let b = 5;
+console.log((Object.is(a, b)))
+
+// 2) Task: Use Object.is to compare two different object instances that have the same properties.
+let obj26 = { name: "Alice" };
+let obj27 = { name: "Alice" };
+console.log(Object.is(obj26, obj27))
+
+// 3) Task: Use Object.is to compare an object with itself.
+let obj28 = { name: "Alice" };
+console.log(Object.is(obj28, obj28))
+
+// 4) Task: Use Object.is to compare "hello" and "Hello".
+console.log(Object.is('hello', 'Hello'))
+
+
+// 4) Task: Use Object.is to compare "hello" and "hello".
+console.log(Object.is('hello', 'hello'))
+
+
+// Object.getPrototypeOf
+
+// 1) Task: Use Object.getPrototypeOf to retrieve the prototype of an object created with Object.create({}).
+let proto = {
+    greet() {
+        return 'hello'
+    }
+}
+let obj29 = Object.create(proto)
+let obj29Proto = Object.getPrototypeOf(obj29)
+console.log(obj29Proto)
+
+
+// Object.setPrototypeOf
+
+// 1) Task: Use Object.setPrototypeOf to change the prototype of an object to { greet() { return "Hello"; } }.
+let proto2 = {
+    greet() {
+        return 'hello' 
+    }
+}
+let obj35  = {}
+Object.setPrototypeOf(obj35, proto2)
+
+console.log(obj35.greet())
